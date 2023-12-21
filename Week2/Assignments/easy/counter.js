ac2a335157190463f83bbe9908933bcaff181918
Ishan-009 <ishanmoorjmalani009@gmail.com>
2023-12-21 18:50:07 +0530

    Week 2 Easy Assignments

--- Week2/Assignments/easy/counter.js
+++ Week2/Assignments/easy/counter.js
@@ -1,3 +1,11 @@
 let counter = 0;
 
-setTimeout();
+setInterval(() => {
+  counter++;
+  console.log("Counter:", counter);
+}, 1000); // Repeat every 1000
+
+
+// setInterval executes the function at every specific interval of time provided in arguments , suppose argument passed is 3 seconds, so it will run function again again at every 3 seconds time interval
+
+
---
+++ Week2/Assignments/easy/writeFile.js
@@ -0,0 +1,8 @@
+// Write File
+
+const fs = require("fs");
+
+fs.writeFileSync(
+  "G:/Harkirat MERN Cohort/Week2/Assignments/easy/b.txt",
+  "Ishan Moorjmalani is a Software Developer"
+);
