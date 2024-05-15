af5e83e4376ad346bade28d68486b4fba451ba4e
Ishan-009 <ishanmoorjmalani009@gmail.com>
2024-05-15 17:25:37 +0530

    Week 16 Live MonoRepo TurboRepo Completed

---
+++ Week16/week16-2/packages/ui/src/admin.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export const AdminButton = () => {
+  return <button>Admin Button</button>;
+};
---
+++ Week16/week16-2/packages/ui/src/input-box.tsx
@@ -0,0 +1,8 @@
+export const InputBox = () => {
+  return (
+    <div>
+      <label htmlFor="">First Name</label>
+      <input type="text" placeholder="Enter your first Name" />
+    </div>
+  );
+};
